caaac8de4c4a382a1292a8592556eadfe747739b
Victor Koome <koomev786@gmail.com>
2023-11-25 13:54:30 +0300

    document the code

--- script.rb
+++ script.rb
@@ -2,31 +2,29 @@ def caesar_cipher(string, num)
 ord_array = []
 ord_array_added = []
 encrypted_array = []
-
+#to downcase to use it easily
 string_array = string.downcase.chars
-
+# loop through the string_array to convert all the charactres to ordinal digits
 for i in string_array do
     ord_array.push(i.ord)
 end
-  
+# loop thr the ord_array to shift the ordinals by num and push them to a new array(ord_array_added)
 for i in ord_array do
     if i < 97
         ord_array_added.push(i)
     elsif (i >= 97) && (i <= 122)
         new_value = i + num
+        #!! special case} if the value is more than 122, 
+        #it subtracts it by 26, wrapping from 122 - 98 in that manner
         new_value -= 26 if new_value > 122
         ord_array_added.push(new_value)
     end
  
 end
-p ord_array_added
+# looping through the final shifted array (ord_array_added)
+# to convert all ordinals to characters
 for i in ord_array_added do
-    if i < 97
-      encrypted_array.push(i.chr)
-    elsif (i >= 97) || (i <= 122)
     encrypted_array.push(i.chr)
-    end
-    
 end
 encrypted_array = encrypted_array.join('')
 p encrypted_array
