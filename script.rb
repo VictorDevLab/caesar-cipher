6736a0ea241d3e6356e8e1a053b3643dbe5fc746
Victor Koome <koomev786@gmail.com>
2023-11-25 13:40:28 +0300

    update the  ordinal values array to adjust numbers more than 122, by subtractin 26

--- script.rb
+++ script.rb
@@ -12,9 +12,12 @@ end
 for i in ord_array do
     if i < 97
         ord_array_added.push(i)
-        else
-        ord_array_added.push(i+=num)
-        end
+    elsif (i >= 97) && (i <= 122)
+        new_value = i + num
+        new_value -= 26 if new_value > 122
+        ord_array_added.push(new_value)
+    end
+ 
 end
 p ord_array_added
 for i in ord_array_added do
@@ -22,12 +25,11 @@ for i in ord_array_added do
       encrypted_array.push(i.chr)
     elsif (i >= 97) || (i <= 122)
     encrypted_array.push(i.chr)
-    else 
-        encrypted_array.push(i)
     end
+    
 end
 encrypted_array = encrypted_array.join('')
 p encrypted_array
 end
 
-caesar_cipher("Hello jacob!", 5)
+caesar_cipher("What a string!", 5)
