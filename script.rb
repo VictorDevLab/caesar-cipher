073a0665c5f5fc50ff7f61669cf6ccfb70e9098a
Victor Koome <koomev786@gmail.com>
2023-11-25 12:53:41 +0300

    add encrypted array to store encrypted characters

--- script.rb
+++ script.rb
@@ -8,11 +8,26 @@ string_array = string.downcase.chars
 for i in string_array do
     ord_array.push(i.ord)
 end
-
+  
 for i in ord_array do
-   ord_array_added.push(i+=num)
+    if i < 97
+        ord_array_added.push(i)
+        else
+        ord_array_added.push(i+=num)
+        end
 end
 p ord_array_added
+for i in ord_array_added do
+    if i < 97
+      encrypted_array.push(i.chr)
+    elsif (i >= 97) || (i <= 122)
+    encrypted_array.push(i.chr)
+    else 
+        encrypted_array.push(i)
+    end
+end
+encrypted_array = encrypted_array.join('')
+p encrypted_array
 end
 
-caesar_cipher("hello", 5)
+caesar_cipher("Hello jacob!", 5)
